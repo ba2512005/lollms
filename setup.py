f6f0cd43fa17ab9718cc36ed1db67040600b5b23
Mohammad Anwar <ba2512005@gmail.com>
2024-12-27 22:56:40 -0500

    Update setup.py
    account for blank lines in requirements

--- setup.py
+++ setup.py
@@ -1,28 +1,34 @@
 from pathlib import Path
 from typing import Union
 
-import setuptools
+setuptools = ...  # assuming you've imported setuptools correctly
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
+README_MD_FILE = "README.md"
+REQUIREMENTS_TXT_FILE = "requirements.txt"
+REQUIREMENTS_DEV_TXT_FILE = "requirements_dev.txt"
 
-
-def read_requirements(path: Union[str, Path]):
+def read_requirements(path: Union[str, Path]) -> list:
+    #accounts for blank lines in req
     with open(path, "r") as file:
-        return file.read().splitlines()
+        return [line.strip() for line in file.readlines()]
 
+requirements = list(filter(None, read_requirements(REQUIREMENTS_TXT_FILE)))
+requirements_dev = list(filter(None, read_requirements(REQUIREMENTS_DEV_TXT_FILE)))
 
-requirements = read_requirements("requirements.txt")
-requirements_dev = read_requirements("requirements_dev.txt")
-
-def get_all_files(path):
+def get_all_files(path: str) -> list:
     path = Path(path)
     file_list = []
-    for file_path in path.rglob('*'):
+    for file_path in path.rglob("*"):
         if file_path.is_file():
-            if file_path.name != "__pycache__" and file_path.suffix !=".pyc" and  file_path.name!="local_config.yaml" and file_path.name!=".installed" and file_path.name!=".git" and file_path.name!=".gitignore":
-                file_list.append("/".join(str(file_path).replace("\\","/").split("/")[1:]))
+            relative_path = file_path.relative_to(path)
+            if not (relative_path.name.startswith("__pycache__") or
+                    relative_path.suffix == ".pyc" or
+                    relative_path.name in ["local_config.yaml", ".installed", ".git", ".gitignore"]):
+                file_list.append(str(relative_path))
     return file_list
+
+with open(README_MD_FILE, "r") as fh:
+    long_description = fh.read()
 
 setuptools.setup(
     name="lollms",
@@ -33,17 +39,15 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/ParisNeo/lollms",
-    packages=setuptools.find_packages(),  
+    packages=setuptools.find_packages(),
     include_package_data=True,
     install_requires=requirements,
     entry_points={
-        'console_scripts': [
-            'lollms-elf = lollms.server.elf:main',
-        ],
+        'console_scripts': ['lollms-elf = lollms.server.elf:main'],
     },
     extras_require={"dev": requirements_dev},
     classifiers=[
-        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3",
         "License :: OSI Approved :: Apache Software License",
         "Operating System :: OS Independent",
     ],
